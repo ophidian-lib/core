1ec1ea43b64e36b529cc69afd48ae688860b4161
PJ <pje@telecommunity.com>
2022-07-16 04:41:23 +0100

    Add 'onElement' function

---
+++ src/dom.ts
@@ -0,0 +1,10 @@
+export function onElement<K extends keyof HTMLElementEventMap>(
+    el: HTMLElement,
+    event: K,
+    selector: string,
+    callback: (this: HTMLElement, ev: HTMLElementEventMap[K], delegateTarget: HTMLElement) => any,
+    options?: boolean | AddEventListenerOptions
+) {
+    el.on(event, selector, callback, options)
+    return () => el.off(event, selector, callback, options);
+}
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export * from "./services";
 export * from "./layout";
 export * from "./defer";
+export * from "./dom";
