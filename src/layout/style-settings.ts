39e77680ead94f8d01cca76caba9ca5f91ace61c
PJ <pje@telecommunity.com>
2022-08-13 05:35:00 +0100

    Improved style settings support

--- src/layout/style-settings.ts
+++ src/layout/style-settings.ts
@@ -1,11 +1,36 @@
+import { around } from "monkey-around";
 import { Service } from "../services";
+import { Plugin } from "obsidian";
 
-/** use() this to make your plugin support Style Settings */
+/** use() this to make your plugin support dynamic Style Settings
+ *
+ * Currently, Obsidian doesn't announce CSS changes when plugins
+ * load, so Style Settings doesn't update when you update a plugin
+ * or install a new one.  This service takes care of that for you,
+ * telling Style Settings to update after a plugin loads its CSS.
+ *
+ * To take effect, this service must be use()d as a plugin property,
+ * or else it will miss the timing to capture the loadCSS event.
+**/
 export class StyleSettings extends Service {
-    onload() { this.triggerReparse(); }
-    onunload() { this.triggerReparse(); }
+    onload() {
+        const self = this, plugin = this.use(Plugin);
+        this.register(around(plugin, {
+            loadCSS(old) { return async function() {
+                await old.call(this);
+                self.triggerReparse();
+                this.register(() => self.triggerReparse())
+            }; }
+        }));
+    }
 
     triggerReparse() {
         if (app.workspace.layoutReady) app.workspace.trigger("parse-style-settings");
+    }
+}
+
+declare module "obsidian" {
+    interface Plugin {
+        loadCSS(): Promise<void>;
     }
 }
