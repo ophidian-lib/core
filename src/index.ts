74a78e973a3c5594ba5f9f38209e8334f59dc71b
PJ <pje@telecommunity.com>
2022-09-16 21:08:06 -0400

    Export declaration for Obsidian's i18next

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,9 @@ export * from "./layout";
 export * from "./defer";
 export * from "./dom";
 export * from "./ui";
+
+// Expose Obsidian's i18n support (type declaration only)
+import { i18n } from "i18next";
+declare global {
+    const i18next: i18n
+}
