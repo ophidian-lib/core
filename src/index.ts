cd9dd600947f8b4ca51cae609c0091a46c2c99dc
PJ <pje@telecommunity.com>
2023-06-05 01:38:41 -0400

    Ensure proper CSS tree-shaking for new UI modules

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,13 @@ export * from "./defer";
 export * from "./dom";
 export * from "./ui";
 
+// These exports use CSS and must be explicitly exported to
+// avoid including the CSS even when they aren't used.
+//
+export {Confirm} from "./ui/confirm";
+export {Dialog} from "./ui/dialog";
+export {Prompt} from "./ui/prompt";
+
 // Expose Obsidian's i18n support (type declaration only)
 import { i18n } from "i18next";
 declare global {
--- src/ui/index.ts
+++ src/ui/index.ts
@@ -1,5 +1,2 @@
 export * from "./commands";
-export * from "./confirm";
-export * from "./dialog";
-export * from "./prompt";
 export * from "./suggest";
