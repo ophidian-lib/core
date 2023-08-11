fad3612abd766358795c38b5bde876df4466fab3
PJ <pje@telecommunity.com>
2023-08-10 22:11:20 -0400

    Support defer in non-browser environment

--- src/defer.ts
+++ src/defer.ts
@@ -1,5 +1,5 @@
 /** Invoke a no-argument function as a microtask, using queueMicrotask or Promise.resolve().then() */
-export const defer: (cb: () => any) => void = window.queueMicrotask ?? (p => (cb: () => any) => p.then(cb))(Promise.resolve());
+export const defer: (cb: () => any) => void = typeof queueMicrotask === "function" ? queueMicrotask : (p => (cb: () => any) => p.then(cb))(Promise.resolve());
 
 /**
  * Return a queuing function that invokes callbacks serially, returning a promise for the task's completion
