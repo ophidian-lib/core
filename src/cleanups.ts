199174098e46c088aed5f822d879fc59a7469be5
PJ <pje@telecommunity.com>
2023-11-02 07:17:34 -0400

    Add abortable async jobs (experimental draft)

--- src/cleanups.ts
+++ src/cleanups.ts
@@ -1,3 +1,6 @@
+import { untracked } from "@preact/signals-core";
+import { defer } from "./defer";
+
 type Nothing = undefined | null | void;
 export type Cleanup = () => unknown;
 export type OptionalCleanup = Cleanup | Nothing;
@@ -50,3 +53,116 @@ function runCleanups(this: CleanupContext) {
     while (this.length) try { this.pop()(); } catch (e) { Promise.reject(e); }
 }
 
+var currentJob: Job<any>;
+
+export type JobGenerator<T=void> = Generator<void,T,any>
+export type Awaiting<T> = Generator<void, T, any>;
+
+/**
+ * Create a new job or fetch the currently-running one
+ *
+ * @param g (Optional) A generator, or a no-argument generator function, that
+ * will run as the job.  If not given, return the current job.  (Note that since
+ * arrow functions can't be generators, functions used to start a job cannot
+ * use `this`.  If you need a job function to have a `this`, make it a method
+ * and call it, or use {@link spawn} instead.)
+ *
+ * @returns A new Job instance, or the current job (which may be undefined).
+ */
+export function job<R>(g?: JobGenerator<R> | ((this:void) => JobGenerator<R>) ): Job<R> {
+    return g ? (new _Job(typeof g === "function" ? g() : g)) : currentJob; }
+
+/**
+ * Create a new job from a context (`this`) and generator function
+ *
+ * @param thisArg The desired `this` for the generator function to receive
+ * @param gf The generator function to be run in the new job.
+ * @returns The new job
+ */
+export function spawn<T,R>(thisArg: T, gf: (this: T) => JobGenerator<R>): Job<R> {
+    return job(gf.call(thisArg) as JobGenerator<R>);
+}
+
+export interface Job<T> extends PromiseLike<T> {
+    next(v?: any): void;
+    return(v?: T): void;
+    throw(e: any): void;
+
+    /** Run a cleanup function when the job ends */
+    cleanup(f: Cleanup): void;
+}
+
+const IS_RUNNING = 1, IS_FINISHED = 2, IS_ERROR = 4 | IS_FINISHED;
+
+class _Job<T> implements Job<T> {
+    constructor(protected g: JobGenerator<T>) {
+        // Ensure the job stops when its parent does
+        const doStop = this.return.bind(this, undefined);
+        cleanup(doStop);
+
+        // As the last step of stopping, remove our doStop callback
+        // from the parent context, so contexts that spawn lots of
+        // jobs don't keep needlessly growing their cleanup lists
+        const parentCtx = cleanupContext;
+        this._ctx.push(() => {
+            const idx = parentCtx.indexOf(doStop);
+            if (idx >= 0) parentCtx.splice(idx, 1);
+        })
+
+        // Start asynchronously
+        defer(() => { this._flags &= ~IS_RUNNING; this.next(); });
+    }
+
+    then<T1=T, T2=never>(
+        onfulfilled?: (value: T) => T1 | PromiseLike<T1>,
+        onrejected?: (reason: any) => T2 | PromiseLike<T2>
+    ): Promise<T1 | T2> {
+        return new Promise((res, rej) => this.cleanup(() => {
+            if ((this._flags & IS_ERROR) === IS_ERROR) rej(this._result); else res(this._result);
+        })).then(onfulfilled, onrejected);
+    }
+
+    next(v?: any)   { if (this.g) this._step("next",   v); }
+    return(v?: T)   { if (this.g) this._step("return", v); }
+    throw(e: any)   { if (this.g) this._step("throw",  e); }
+
+
+    cleanup(f: Cleanup) {
+        this._ctx.push(f);
+        // Already closed?  Defer it to run anyway
+        if (this._ctx.length === 1 && !this.g) defer(runCleanups.bind(this._ctx));
+    }
+
+    // === Internals === //
+
+    protected readonly _ctx: CleanupContext = [];
+    protected _flags = IS_RUNNING;
+    protected _result: any
+
+    protected _step(method: "next" | "throw" | "return", arg: any) {
+        // Don't resume a job while it's running
+        if (this._flags & IS_RUNNING) return defer(this._step.bind(this, method, arg));
+        const oldCtx = cleanupContext;
+        try {
+            this._flags |= IS_RUNNING;
+            currentJob = this;
+            cleanupContext = this._ctx;
+            try {
+                const res: IteratorResult<any, T> = untracked(this.g[method].bind(this.g, arg));
+                if (!res.done) return;
+                this._result = res.value;
+                this._flags |= IS_FINISHED;
+            } catch(e) {
+                this._result = e;
+                this._flags |= IS_ERROR
+            }
+            // Generator returned or threw: ditch it and run cleanups
+            this.g = undefined;
+            runCleanups.call(this._ctx);
+        } finally {
+            currentJob = undefined;
+            cleanupContext = oldCtx;
+            this._flags &= ~IS_RUNNING;
+        }
+    }
+}
