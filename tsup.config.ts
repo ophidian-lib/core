41c3ab53562feecfc3d7529fd110534117a220bc
PJ <pje@telecommunity.com>
2024-05-06 16:15:40 -0400

    Build with tsup

---
+++ tsup.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from 'tsup';
+import inline from "esbuild-plugin-inline-import";
+import { dirname } from "path";
+import * as sass from "sass";
+
+export default defineConfig({
+    entry: ["src/index.ts"],
+    sourcemap: true,
+    format: ["cjs", "esm"],
+    treeshake: true,
+    dts: true,
+    clean: true,
+    esbuildPlugins: [
+        // sassPlugin(),
+        inline({filter: /^text:/}),
+        inline({filter: /^scss:/, transform(data, args) {
+            return new Promise((resolve, reject) => {
+                sass.render({data, includePaths: [dirname(args.path)]}, (err, result) => {
+                    if (err) return reject(err);
+                    resolve(result.css.toString());
+                });
+            });
+        }}),
+    ],
+})
